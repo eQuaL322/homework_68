344a0307ebc41750916081de111f891b6951a44a
Alima Utkelbayeva <alima150201@gmail.com>
2023-04-15 04:31:37 +0600

    #6 Добавлены  ссылки

--- source/urls.py
+++ source/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path("auth/", include('accounts.urls')),
+                  path("", include('headhunter.urls')),
 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
