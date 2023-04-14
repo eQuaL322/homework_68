06ed76e964ca80539978190130bbc99eb506b300
Alima Utkelbayeva <alima150201@gmail.com>
2023-04-15 04:26:36 +0600

    #2 Добавлены ссылки

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accounts.views import LoginView, logout_view, RegisterView
+from accounts.views import LoginView, logout_view, RegisterView, ProfileView, ProfileChangeView, PasswordChangeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/change/', ProfileChangeView.as_view(), name='profile_change'),
+    path('profile/password-change/', PasswordChangeView.as_view(), name='password_change'),
 ]
