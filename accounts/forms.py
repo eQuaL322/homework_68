3fc505269e606d9bce687c36532c12b032ffdc84
Alima Utkelbayeva <alima150201@gmail.com>
2023-04-15 04:25:18 +0600

    #2 Созданы формы редактирования пользователя и смены пароля

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from accounts.models import UserTypeChoice
 
@@ -64,3 +65,58 @@ class CustomUserCreationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'avatar'
+        )
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Пароль',
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput
+    )
+    password_confirm = forms.CharField(
+        label='Подтвердите пароль',
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput
+    )
+    old_password = forms.CharField(
+        label="Старый пароль",
+        strip=False,
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Старый пароль неверный!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ['password', 'password_confirm', 'old_password']
