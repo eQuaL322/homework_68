beaac63cfb8ebdc8fc59d87c6e9ea0a174cdef38
Alima Utkelbayeva <alima150201@gmail.com>
2023-04-21 21:37:30 +0600

    #8 добавлены ссылки

--- headhunter/urls.py
+++ headhunter/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from headhunter.views.resume import ResumeUpdateDateView, ResumeDetailView, CreateResumeView, UpdateResumeView
 from headhunter.views.education import EducationCreateView, EducationUpdateView
 from headhunter.views.experience import CreateExperienceView, ExperienceUpdateView
+from headhunter.views.vacancy import CreateVacancyView, VacancyDetailView, UpdateVacancyView
 
 urlpatterns = [
     path('resume/<int:pk>/update/date/', ResumeUpdateDateView.as_view(), name='resume_update_date'),
@@ -13,4 +14,7 @@ urlpatterns = [
     path('resume/<int:pk>/edit', UpdateResumeView.as_view(), name='resume_edit'),
     path('resume/<int:pk>/experience/edit/', ExperienceUpdateView.as_view(), name='experience_edit'),
     path('resume/<int:pk>/education/edit/', EducationUpdateView.as_view(), name='education_edit'),
+    path('vacancy/<int:pk>/update/date/', UpdateVacancyView.as_view(), name='vacancy_edit'),
+    path('vacancy/<int:pk>/detail', VacancyDetailView.as_view(), name='vacancy_detail'),
+    path('vacancy/create/', CreateVacancyView.as_view(), name='vacancy_create'),
 ]
